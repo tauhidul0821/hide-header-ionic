0ce0ac82baea1ba930b801956ffd3f7c4bc02e54
tauhidbs23 <tauhidul.islam@brainstation23.com>
2021-02-04 15:13:55 +0600

    added fade header

--- src/app/directives/fade-header.directive.ts
+++ src/app/directives/fade-header.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Input, Renderer2 } from '@angular/core';
+import { DomController } from '@ionic/angular';
 
 @Directive({
   selector: '[appFadeHeader]'
 })
 export class FadeHeaderDirective {
+  @Input('appFadeHeader') toolbar: any;
 
-  constructor() { }
+
+  constructor(private domCtrl: DomController) { }
+
+  ngOnInit(): void {
+    this.toolbar = this.toolbar.el;
+  }
+
+  @HostListener('ionScroll',['$event']) onContentScroll($event){
+    let scrollTop = $event.detail.scrollTop;
+    if(scrollTop >= 255){
+      scrollTop = 255;
+    }
+
+    const hexDist = scrollTop.toString(16);
+
+    this.domCtrl.write(()=>{
+      this.toolbar.style.setProperty('--background',`#f4f5f8${hexDist}`)
+    });
+
+  }
 
 }
