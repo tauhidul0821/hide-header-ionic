88a3be643399b69351ff1fa2c62501e6fb23a3f6
tauhidbs23 <tauhidul.islam@brainstation23.com>
2021-02-04 14:32:58 +0600

    hide header using directives

---
+++ src/app/directives/hide-header.directive.ts
@@ -0,0 +1,37 @@
+import { Directive, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
+import { DomController } from '@ionic/angular';
+
+@Directive({
+  selector: '[appHideHeader]'
+})
+export class HideHeaderDirective implements OnInit {
+  @Input('appHideHeader') toolbar: any;
+  private toolbarHeight = 44;
+
+
+  constructor(private renderer: Renderer2, private domCtrl: DomController) { }
+
+  ngOnInit(): void {
+    this.toolbar = this.toolbar.el;
+    this.domCtrl.read(()=>{
+      this.toolbarHeight = this.toolbar.clientHeight;
+    });
+  }
+
+  @HostListener('ionScroll',['$event']) onContentScroll($event){
+    const scrollTop = $event.detail.scrollTop;
+    let newPosition = - (scrollTop / 5);
+
+    if(newPosition < -this.toolbarHeight){
+      newPosition = -this.toolbarHeight;
+    }
+
+    let newOpacity = 1 - (newPosition / -this.toolbarHeight);
+
+    this.domCtrl.write(()=>{
+      this.renderer.setStyle(this.toolbar,'top',`${newPosition}px`)
+      this.renderer.setStyle(this.toolbar,'opacity',newOpacity)
+    });
+  }
+
+}
--- src/app/directives/shared-directives.module.ts
+++ src/app/directives/shared-directives.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HideHeaderDirective } from './hide-header.directive';
+import { FadeHeaderDirective } from './fade-header.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HideHeaderDirective, FadeHeaderDirective],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [HideHeaderDirective, FadeHeaderDirective],
 })
 export class SharedDirectivesModule { }
